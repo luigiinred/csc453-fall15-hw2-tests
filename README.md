845e63f13c38c347e8bd80dc33eb8a23d599e8c9
Timmy Garrabrant <timmy.garrabrant@gmail>
2015-09-10 17:40:41 -0700

    declarations

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# csc453-fall15-hw2-tests
+# csc453-fall15-hw1-2-tests
+
+
+## Running tests
+
+Clone this into your project's root
+
+```sh
+git clone git://github.com/luigiinred/csc453-fall15-hw1-2-tests.git
+```
+
+Add this command for myhtml2txt:
+
+```sh
+(cd csc453-fall15-hw1-2-tests && ./run.zsh)
+```
+
+Your output and the output of diff will be saved into each test's directory.
+
+
+Part 2's myhtml2txt executable is not up yet. I will generate failing tests once I get that.  
+
+
+
+## Errors
+
+Each test will have either test.out or test.err
+
+If there is an test.err file in a test's directory I will assume the exit status should be 1
+
+I am assuming all errors are formatted "Error on line x" (I will change this once I think of something I like)
---
+++ run.zsh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+for d in */ ; do
+
+
+    echo -e "\n============ $d ============"
+
+    ../compile < $d/test.in > $d/out.txt 2> $d/err.out
+    exit_code=$?
+
+    # I am expecting an error
+    if [ -f $d/test.err ];
+    then
+
+      # I get an Error
+      if [[ $exit_code -eq 1 ]]; then
+        diff -c $d/err.out $d/test.err >$d/diff.out 2>&1
+        if [ $? -eq 0 ]
+        then
+            echo "$d Passed"
+        else
+          echo -e "$d Failed\Errors do not match \ndiff.out:\n"
+          cat $d/diff.out
+          echo -e "\n"
+        fi
+
+      # I do not get an error
+      else
+        echo -e "$d Failed\nNo error when expected\nout.txt:\n"
+              cat $d/out.txt
+
+      fi
+
+    # I am not expecting an error
+    else
+      # I get an Error
+      if [[ $exit_code -eq 1 ]]; then
+        echo -e "$d Failed\nUnexpected Error\nerr.out:\n"
+        cat $d/err.out
+        echo -e "\n"
+
+      # I do not get an error
+      else
+        echo "$d Passed"
+
+      fi
+    fi
+
+done
